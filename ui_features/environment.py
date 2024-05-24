4a6e214e70dc3a166b1c61116c43f2514a11c603
DuyguT88 <duygu.baydi@gmail.com>
2024-05-24 23:53:55 +0200

    fixed display number

--- ui_features/environment.py
+++ ui_features/environment.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def before_all(context):
     context.base_url = "http://www.uitestingplayground.com"
 
 def before_scenario(context, scenario):
-    context.driver = webdriver.Chrome()  # or use another WebDriver
-    context.driver.maximize_window()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920x1080")
+    context.driver = webdriver.Chrome(options=chrome_options)
     context.base_url = "http://www.uitestingplayground.com"
 
 def after_scenario(context, scenario):
